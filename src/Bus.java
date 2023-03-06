a8d7cc27c89d1f786d4ca282d0623c00a801381f
Valentyna Kondrashenko <oreshkav@gmail.com>
2023-03-07 00:39:35 +0100

    add lesson35hw3

---
+++ src/Bus.java
@@ -0,0 +1,26 @@
+public class Bus {
+  //    //Задача 3 (*) Создать класс Bus (автобус) - с полями
+  //    // номер маршрута, вместимость, скорость на маршруте, длина маршрута.
+  //    // Реализовать для него методы go, stop, speedUp, speedDouwn
+
+  String number;
+  String qapasity;
+  int speed;
+  int length;
+
+  void go() {
+    System.out.println("Method go()");
+  }
+
+  void stop(){
+    System.out.println("Method stop()");
+  }
+
+  void speedUp(){
+    System.out.println("Method speedUp()");
+  }
+
+  void speedDown(){
+    System.out.println("Method speedDown()");
+  }
+}
---
+++ src/lesson35hw3.java
@@ -0,0 +1,14 @@
+public class lesson35hw3 {
+  public static void main(String[] args){
+    //Задача 3 (*) Создать класс Bus (автобус) - с полями
+    // номер маршрута, вместимость, скорость на маршруте, длина маршрута.
+    // Реализовать для него методы go, stop, speedUp, speedDouwn
+
+    Bus bus = new Bus();
+
+    bus.go();
+    bus.stop();
+    bus.speedUp();
+    bus.speedDown();
+  }
+}
